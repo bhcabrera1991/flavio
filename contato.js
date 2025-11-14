5e7f3bd2fbc841908abc3c64fc38831ddc2b1ab6
bhcabrera1991 <brunohenriquecabrera@gmail.com>
2025-11-14 09:42:56 -0300

    Atualizado o o JS da pagina CONTATO

--- contato.js
+++ contato.js
@@ -1,8 +1,26 @@
 function fn(){
-    let contato =0
-    if(contato >=0){
-        alert("Numero maior que 3")
+    let recado ='';
+    recado=document.getElementById('recado').value
+    mensagemEscrita= document.getElementById('mensagem')
+    if(mensagemEscrita.value.trim()!==""){
+        alert("Recebemos sua mensagem. Entraremos em contato o mais RAPIDO POSSIVEL")
     }else{
-        alert("Errou")
+        alert("Nao entendi sua pergunta. REENVIE NOVAMENTE ")
     }
+
+   
 }
+
+
+
+/*function fnJogo(){
+    let chute = 0, numeroMagico = Math.floor(Math.random()*10)
+    chute=document.getElementById("valor").value
+    if(chute == numeroMagico){
+        document.getElementById("resultado").innerText="Acertou"
+    }else{
+        document.getElementById("resultado")
+        alert("Errou " +numeroMagico)
+    }
+}*/
+
