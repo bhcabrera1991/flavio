d85df2c7b8d48718c4a20c046c223f3a4331cedc
bhcabrera1991 <brunohenriquecabrera@gmail.com>
2025-11-15 10:29:52 -0300

    atualizei a pagina html Contato  e ainda formatndo a pagina contato.js com comandos

--- contato.js
+++ contato.js
@@ -1,11 +1,14 @@
 function fn(){
-    let recado ='';
-    recado=document.getElementById('recado').value
+    let mensagemEscrita=''; contato=0; nome='';e_mail=''
+    nome = document.getElementById('nome')
+    e_mail= document.getElementById('e_mail')
+    contato=document.getElementById('contato')
     mensagemEscrita= document.getElementById('mensagem')
-    if(mensagemEscrita.value.trim()!==""){
-        alert("Recebemos sua mensagem. Entraremos em contato o mais RAPIDO POSSIVEL")
-    }else{
-        alert("Nao entendi sua pergunta. REENVIE NOVAMENTE ")
+    if(mensagemEscrita.value.trim()!=="" && contato.value>1111111111 && e_mail.value.trim()!="" && nome.value.trim()!=""){
+        alert("Recebemos sua mensagem. Entraremos em contato o mais RAPIDO POSSIVEL no telefone " + contato.value)
+    }else if (mensagemEscrita=value.trim()!= && contato.value<11111111110) {
+        alert("Vamos entrar em contato pelo email " + e_mail)
+        
     }
 
    
