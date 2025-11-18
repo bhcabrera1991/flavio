236dc1fb506c101d865f40698f952bb803188cf1
bhcabrera1991 <brunohenriquecabrera@gmail.com>
2025-11-17 23:33:17 -0300

    Ediatando arquvio CONTATO.JS

--- contato.js
+++ contato.js
@@ -1,8 +1,12 @@
 function fn(){
-    let mensagemEscrita='';      
+    let mensagemEscrita='';
+    let contato= 0
+    contato= document.getElementById('contato')      
     mensagemEscrita= document.getElementById('mensagem')
-    if(mensagemEscrita.value.trim()!==""){
+    if(mensagemEscrita.value.trim()!=="" && contato.value >11111111111){
         alert("Recebemos sua mensagem. Entraremos em contato o mais RAPIDO POSSIVEL no telefone " + contato.value)
+    }else{
+        alert("Os campos nao foram completamente preenchido. NAO RECEBEMOS SUA DUVIDA. Tente novamente")
     }
 
    
